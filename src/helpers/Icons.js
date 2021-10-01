bfb370129d07a5355eb7da606f30ab6bacc19b0f
Yaiza <yaizavallejo16@gmail.com>
2021-10-01 16:45:41 +0200

    Add arrow icons

--- src/helpers/Icons.js
+++ src/helpers/Icons.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IconContext } from 'react-icons';
 import { BiCameraMovie, BiSearch, BiX } from 'react-icons/bi';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 
 const IconMovie = () => (
   <IconContext.Provider value={{ size: '40px', color: '#ce2e27' }}>
@@ -17,6 +18,18 @@ export const IconSearch = () => (
 export const IconClose = () => (
   <IconContext.Provider value={{ size: '32px', color: '#f6f6f6' }}>
     <BiX />
+  </IconContext.Provider>
+);
+
+export const IconArrowRight = () => (
+  <IconContext.Provider value={{ size: '50px', color: '#f6f6f6' }}>
+    <MdKeyboardArrowLeft />
+  </IconContext.Provider>
+);
+
+export const IconArrowLeft = () => (
+  <IconContext.Provider value={{ size: '50px', color: '#f6f6f6' }}>
+    <MdKeyboardArrowRight />
   </IconContext.Provider>
 );
 
