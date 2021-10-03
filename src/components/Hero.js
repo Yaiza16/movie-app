273db498e04924c527d594c01e7d00314151b1a9
Yaiza <yaizavallejo16@gmail.com>
2021-10-03 17:49:32 +0200

    Add styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import requests from '../helpers/requests';
+import './Hero.scss';
+
+const baseUrlImage = 'https://image.tmdb.org/t/p/original';
 
 const Hero = () => {
   const [movie, getMovie] = useState([]);
@@ -9,19 +12,31 @@ const Hero = () => {
     const fecthData = async () => {
       const request = await axios.get(requests.fetchSeriesPopularity);
       const lengthRequest = request.data.results.length;
-      const option = Math.floor(Math.random() * lengthRequest - 1);
-      getMovie(request.data.results[option]);
-      console.log(request.data.results[option]);
-      //   console.log(request);
-      //   console.log(lengthRequest);
-      //   console.log(option);
+      let optionRandom;
+      let option;
+      do {
+        optionRandom = Math.floor(Math.random() * lengthRequest - 1);
+        option = request.data.results[optionRandom].backdrop_path;
+      } while (option === undefined);
+      getMovie(request.data.results[optionRandom]);
+      console.log(request.data.results[optionRandom]);
     };
     fecthData();
   }, []);
 
   return (
     <div className="hero-container">
-      <div className="h1">{movie.name || movie.original_name}</div>
+      <div className="hero-img-container">
+        <img
+          className="hero-img"
+          src={`${baseUrlImage}${movie.backdrop_path}`}
+          alt="{movie.name || movie.original_name}"
+        />
+      </div>
+      <div className="hero-inf">
+        <h1 className="hero-inf--title">{movie.name || movie.original_name}</h1>
+        <p className="hero-inf--overview">{movie.overview}</p>
+      </div>
     </div>
   );
 };
