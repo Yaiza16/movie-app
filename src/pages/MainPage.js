f918f6fee0e06e5faf910577eec57116532f44d8
Yaiza <yaizavallejo16@gmail.com>
2021-10-03 16:50:40 +0200

    Add Hero

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Hero from '../components/Hero';
 import Slider from '../components/Slider';
 import requests from '../helpers/requests';
 
 const MainPage = () => {
   return (
     <div>
+      <Hero />
       <Slider title="Trending now" fetchUrl={requests.fetchTrendies} main />
       <Slider
         title="Top Rated Movies"
