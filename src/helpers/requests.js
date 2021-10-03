150aa8317c56b8e7aea51d0ce00b3cd37ceaa535
Yaiza <yaizavallejo16@gmail.com>
2021-10-03 11:50:45 +0200

    add new request

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -2,6 +2,8 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
   fetchTrendies: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`,
+  fetchMoviesTopRated: `
+  https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
 };
 
 export default requests;
