30d6c805d1952dfb4b1c43d7226e52d9eb626521
Yaiza <yaizavallejo16@gmail.com>
2021-10-04 18:46:14 +0200

    Add series page to router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import MainPage from '../pages/MainPage';
 import MoviesPage from '../pages/MoviesPage';
+import SeriesPage from '../pages/SeriesPage';
 
 const AppRouter = () => {
   return (
@@ -16,7 +17,7 @@ const AppRouter = () => {
           <MoviesPage />
         </Route>
         <Route path="/series">
-          <h1>Movies</h1>
+          <SeriesPage />
         </Route>
         <Route exact path="/">
           <MainPage />
