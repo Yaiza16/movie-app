80cafb363d2a461b353a3e3896dc988c0f1a729e
Yaiza <yaizavallejo16@gmail.com>
2021-10-06 16:18:13 +0200

    Fix problem related to scroll's position when navigate to another page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
+import ScrollToTop from '../hooks/ScrollToTop';
 import MainPage from '../pages/MainPage';
 import MoviesPage from '../pages/MoviesPage';
 import SeriesPage from '../pages/SeriesPage';
 
 const AppRouter = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoading = () => {
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('load', handleLoading);
+    return () => window.removeEventListener('load', handleLoading);
+  }, []);
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route path="/search">
-          <h1>Search</h1>
-        </Route>
-        <Route path="/movies">
-          <MoviesPage />
-        </Route>
-        <Route path="/series">
-          <SeriesPage />
-        </Route>
-        <Route exact path="/">
-          <MainPage />
-        </Route>
-      </Switch>
+      <ScrollToTop>
+        <Header />
+        {isLoading ? (
+          <h1>Hola</h1>
+        ) : (
+          <Switch>
+            <Route path="/search">
+              <h1>Search</h1>
+            </Route>
+            <Route path="/movies">
+              <MoviesPage />
+            </Route>
+            <Route path="/series">
+              <SeriesPage />
+            </Route>
+            <Route exact path="/">
+              <MainPage />
+            </Route>
+          </Switch>
+        )}
+      </ScrollToTop>
     </Router>
   );
 };
