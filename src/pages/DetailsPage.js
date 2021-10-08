720a84add318dcbe386714390d2b61373214bc18
Yaiza <yaizavallejo16@gmail.com>
2021-10-08 18:13:33 +0200

    Set up page

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const DetailsPage = () => {
-  return <div />;
+  return (
+    <div className="details-page-container">
+      <div className="details-page">
+        <div className="details-page__poster-container">
+          <img src="" alt="" className="details-page__poster" />
+        </div>
+        <div className="details-page__inf">
+          <h2 className="title-movie">Hola</h2>
+          <p className="genres-movie">Genre: </p>
+          <p className="overview-movie">Overview here</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailsPage;
