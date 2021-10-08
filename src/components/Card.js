824d041de5659bff9c810fbcb297a98fd7442f3d
Yaiza <yaizavallejo16@gmail.com>
2021-10-08 18:13:14 +0200

    Add link to movieDetails to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,41 +1,44 @@
 import React from 'react';
 import './Card.scss';
 import { SwiperSlide } from 'swiper/react';
+import { Link } from 'react-router-dom';
 
 const baseUrlImage = 'https://image.tmdb.org/t/p/original';
 
 const Card = ({ movie, main }) => {
   return (
-    <div className="swipper-wrapped">
-      {main ? (
-        <SwiperSlide className="movie-card-container">
-          <img
-            className="movie-poster"
-            src={`${baseUrlImage}${movie.poster_path} `}
-            alt={movie.title || movie.name}
-          />
-        </SwiperSlide>
-      ) : (
-        <SwiperSlide className="movie-card-container movie-card-container--back">
-          <div className="card-info">
-            <p className="card-info-title">{movie.title || movie.name}</p>
-          </div>
-          <img
-            className="movie-poster"
-            // src={
-            //   `${baseUrlImage}${movie.backdrop_path} ` ||
-            //   `{process.env.PUBLIC_URL} + /assets/defaultimage.png`
-            // }
-            src={
-              movie.backdrop_path
-                ? `${baseUrlImage}${movie.backdrop_path}`
-                : `${process.env.PUBLIC_URL}/assets/defaultimage.png`
-            }
-            alt={movie.title || movie.name}
-          />
-        </SwiperSlide>
-      )}
-    </div>
+    <Link to={`/movie/${movie.id}`}>
+      <div className="swipper-wrapped">
+        {main ? (
+          <SwiperSlide className="movie-card-container">
+            <img
+              className="movie-poster"
+              src={`${baseUrlImage}${movie.poster_path} `}
+              alt={movie.title || movie.name}
+            />
+          </SwiperSlide>
+        ) : (
+          <SwiperSlide className="movie-card-container movie-card-container--back">
+            <div className="card-info">
+              <p className="card-info-title">{movie.title || movie.name}</p>
+            </div>
+            <img
+              className="movie-poster"
+              // src={
+              //   `${baseUrlImage}${movie.backdrop_path} ` ||
+              //   `{process.env.PUBLIC_URL} + /assets/defaultimage.png`
+              // }
+              src={
+                movie.backdrop_path
+                  ? `${baseUrlImage}${movie.backdrop_path}`
+                  : `${process.env.PUBLIC_URL}/assets/defaultimage.png`
+              }
+              alt={movie.title || movie.name}
+            />
+          </SwiperSlide>
+        )}
+      </div>
+    </Link>
   );
 };
 
