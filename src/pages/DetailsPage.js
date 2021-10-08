6f054de03c05073b905a29aec43fbb879a1b0746
Yaiza <yaizavallejo16@gmail.com>
2021-10-08 18:19:49 +0200

    Add useParams

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const DetailsPage = () => {
+  const { id } = useParams();
+  console.log(id);
   return (
     <div className="details-page-container">
       <div className="details-page">
