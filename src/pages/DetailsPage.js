8d4a684d722a91eda7fbe0825cf5a4ad219a577a
Yaiza <yaizavallejo16@gmail.com>
2021-10-11 10:38:25 +0200

    Add recommendations cards

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import './DetailsPage.scss';
+import Card from '../components/Card';
 
 const baseUrlImage = 'https://image.tmdb.org/t/p/original';
 
 const DetailsPage = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState([]);
+  const [recommendation, setRecommendation] = useState([]);
   console.log(id);
 
   useEffect(() => {
@@ -22,7 +24,19 @@ const DetailsPage = () => {
 
       return request;
     }
+    async function catchRecommendation() {
+      const requestRecommendation = await axios.get(
+        `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=9650301cf84b0f134776248aa6863452&language=en-US&page=1`
+      );
+      if (requestRecommendation.data.results.length >= 4) {
+        setRecommendation(requestRecommendation.data.results.slice(0, 4));
+      } else {
+        setRecommendation(requestRecommendation.data.results);
+      }
+      return requestRecommendation;
+    }
     catchMovie();
+    catchRecommendation();
   }, [id]);
 
   return (
@@ -37,17 +51,28 @@ const DetailsPage = () => {
         </div>
         <div className="details-page__inf">
           <h2 className="title-movie">{movie.title}</h2>
-          <p className="genres-movie">
-            {movie.genres &&
-              movie.genres.map((genre, i, arr) => {
-                if (arr.length - 1 === i) {
-                  return `${genre.name} `;
-                }
-                return `${genre.name}, `;
-              })}
-          </p>
+          <div className="details-container">
+            <p className="details-container__title">Genre: </p>
+            <p className="details-container__inf genres-movie">
+              {movie.genres &&
+                movie.genres.map((genre, i, arr) => {
+                  if (arr.length - 1 === i) {
+                    return `${genre.name} `;
+                  }
+                  return `${genre.name}, `;
+                })}
+            </p>
+          </div>
 
           <p className="overview-movie">{movie.overview}</p>
+          <div className="recommendations">
+            <p className="recommendations-title">Recommendations: </p>
+            <div className="recommendations-cards-container">
+              {recommendation.map((rec) => {
+                return <Card movie={rec} key={rec.id} main={false} />;
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
