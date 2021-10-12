c1bce097e5106e21eb1b06e0d24efd04075a08c3
Yaiza <yaizavallejo16@gmail.com>
2021-10-12 10:35:33 +0200

    Create function to get movieCast

---
+++ src/helpers/movieInfo/getMovieCast.js
@@ -0,0 +1,22 @@
+const getDirectors = (data) => {
+  return data.filter((person) => person.job === 'Director');
+};
+
+const getScreenPlayers = (data) => {
+  return data.filter((person) => person.job === 'Screenplay');
+};
+
+const getWriters = (data) => {
+  return data.filter((person) => person.job === 'Story');
+};
+
+const getMovieCast = (data) => ({
+  cast: data.cast,
+  crew: [
+    { directors: getDirectors(data.crew) },
+    { screenPlayers: getScreenPlayers(data.crew) },
+    { writers: getWriters(data.crew) },
+  ],
+});
+
+export default getMovieCast;
