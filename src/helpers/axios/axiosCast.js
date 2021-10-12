31cb6467b829414ab5666105897ac39795858d13
Yaiza <yaizavallejo16@gmail.com>
2021-10-12 11:00:09 +0200

    Add axios for every needed API's call

---
+++ src/helpers/axios/axiosCast.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import baseRequest from './axiosBase';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const axiosCast = async (id, type) => {
+  const res = await axios.get(
+    `${baseRequest}/${type}/${id}/credits?api_key=${API_KEY}`
+  );
+
+  const { data } = res;
+  return data;
+};
+
+export default axiosCast;
