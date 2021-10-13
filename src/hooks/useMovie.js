edc069872670de9d4cc87c599ea4b74fd6c9a569
Yaiza <yaizavallejo16@gmail.com>
2021-10-13 18:53:12 +0200

    Remove comments

--- src/hooks/useMovie.js
+++ src/hooks/useMovie.js
@@ -6,18 +6,12 @@ import getVideo from '../helpers/movieInfo/getVideo';
 
 const useMovie = () => {
   const getNewMovie = async (id, type) => {
-    // const movieCast = getMovieCast(await axiosCast(id, type));
-    // const movieMainData = getMovieMainData(await axiosMainData(id, type));
-    // const movieRecommendations = getRecommendations(
-    //   await axiosRecommendations(id, type)
-    // );
     const movieCast = getMovieCast(await axiosRequest(id, type, 'credits'));
     const movieMainData = getMovieMainData(await axiosRequest(id, type));
     const movieRecommendations = getRecommendations(
       await axiosRequest(id, type, 'recommendations')
     );
     const video = getVideo(await axiosRequest(id, type, 'videos'));
-    // const video = getVideo(await axiosVideo(id, type));
 
     console.log(movieRecommendations);
 
