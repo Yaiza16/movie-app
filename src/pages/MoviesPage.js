f7c4f0f420301e182b2d9bb764d083d73641620c
Yaiza <yaizavallejo16@gmail.com>
2021-10-13 14:22:07 +0200

    Add media's prop to sliders

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -10,36 +10,43 @@ const MoviesPage = () => {
         title="Action Movies"
         fetchUrl={requests.fetchMoviesAction}
         main={false}
+        media="movie"
       />
       <Slider
         title="Romance Movies"
         fetchUrl={requests.fetchMoviesRomance}
         main={false}
+        media="movie"
       />
       <Slider
         title="Fantasy Movies"
         fetchUrl={requests.fetchMoviesFantasy}
         main={false}
+        media="movie"
       />
       <Slider
         title="History Movies"
         fetchUrl={requests.fetchMoviesHistory}
         main={false}
+        media="movie"
       />
       <Slider
         title="Horror Movies"
         fetchUrl={requests.fetchMoviesHorror}
         main={false}
+        media="movie"
       />
       <Slider
         title="Drama Movies"
         fetchUrl={requests.fetchMoviesDrama}
         main={false}
+        media="movie"
       />
       <Slider
         title="Science Fiction Movies"
         fetchUrl={requests.fetchMoviesScienceFiction}
         main={false}
+        media="movie"
       />
     </div>
   );
--- src/pages/SeriesPage.js
+++ src/pages/SeriesPage.js
@@ -9,41 +9,49 @@ const SeriesPage = () => {
         title="Crime Series"
         fetchUrl={requests.fetchSeriesCrime}
         main={false}
+        media="tv"
       />
       <Slider
         title="Animation Series"
         fetchUrl={requests.fetchSeriesAnimation}
         main={false}
+        media="tv"
       />
       <Slider
         title="Drama Series"
         fetchUrl={requests.fetchSeriesDrama}
         main={false}
+        media="tv"
       />
       <Slider
         title="Comedy Series"
         fetchUrl={requests.fetchSeriesComedy}
         main={false}
+        media="tv"
       />
       <Slider
         title="Reality TV Shows"
         fetchUrl={requests.fetchSeriesReality}
         main={false}
+        media="tv"
       />
       <Slider
         title="Action & Adventure Series"
         fetchUrl={requests.fetchSeriesAction}
         main={false}
+        media="tv"
       />
       <Slider
         title="Kids Series"
         fetchUrl={requests.fetchSeriesKids}
         main={false}
+        media="tv"
       />
       <Slider
         title="Mistery Series"
         fetchUrl={requests.fetchSeriesMistery}
         main={false}
+        media="tv"
       />
     </div>
   );
