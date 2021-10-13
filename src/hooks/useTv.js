c0ef8c34f32aecd7a8f1c00f59b5d8ab99f10e7f
Yaiza <yaizavallejo16@gmail.com>
2021-10-13 16:02:33 +0200

    Change names const in order to match with useMovie

--- src/hooks/useTv.js
+++ src/hooks/useTv.js
@@ -1,12 +1,26 @@
+import axiosCast from '../helpers/axios/axiosCast';
 import axiosMainData from '../helpers/axios/axiosMainData';
+import axiosRecommendations from '../helpers/axios/axiosRecommendations';
+import axiosVideo from '../helpers/axios/axiosVideo';
+import getRecommendations from '../helpers/movieInfo/getRecommendations';
+import getTvCast from '../helpers/movieInfo/getTvCast';
 import getTvMainData from '../helpers/movieInfo/getTvMainData';
+import getVideo from '../helpers/movieInfo/getVideo';
 
 const useTv = () => {
   const getNewTv = async (id, type) => {
     const tvMainData = getTvMainData(await axiosMainData(id, type));
+    const tvCast = getTvCast(await axiosCast(id, type));
+    const video = getVideo(await axiosVideo(id, type));
+    const movieRecommendations = getRecommendations(
+      await axiosRecommendations(id, type)
+    );
 
     const variable = {
       ...tvMainData,
+      ...tvCast,
+      video,
+      movieRecommendations,
     };
     console.log(variable);
 
