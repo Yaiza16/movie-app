19a95086747a4d91d2c63b5245cc3311b1d6de3a
Yaiza <yaizavallejo16@gmail.com>
2021-10-15 11:45:54 +0200

    Sort recommendations to actors by popularity of movies they played

--- src/helpers/movieInfo/getRecommendations.js
+++ src/helpers/movieInfo/getRecommendations.js
@@ -1,5 +1,16 @@
+const orderRecommendations = (data) => {
+  const recommendations = data.cast.sort((a, b) => {
+    if (a.popularity > b.popularity) return -1;
+    return a.popularity < b.popularity ? 1 : 0;
+  });
+  return recommendations;
+};
+
 const getRecommendations = (data) => data.results;
 
-export const getRecommendationsPerson = (data) => data.cast;
+export const getRecommendationsPerson = (data) => {
+  const recommendations = orderRecommendations(data);
+  return recommendations;
+};
 
 export default getRecommendations;
