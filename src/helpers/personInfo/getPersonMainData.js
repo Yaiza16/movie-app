e9b30d2192bafb6c366e45e3c59e358036019a14
Yaiza <yaizavallejo16@gmail.com>
2021-10-15 11:15:41 +0200

    Add function to get birthday with format

--- src/helpers/personInfo/getPersonMainData.js
+++ src/helpers/personInfo/getPersonMainData.js
@@ -1,7 +1,22 @@
+const getBirthday = (birthday) => {
+  const today = new Date();
+  const birthDate = new Date(birthday);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  const birthDay = birthDate.getDate();
+  const birthMonth = birthDate.toLocaleString('en-US', { month: 'short' });
+  const birthYear = birthDate.getFullYear();
+  return `${birthDay} ${birthMonth} ${birthYear} (${age} aged)`;
+};
+
 const getPersonMainData = (data) => ({
   name: data.name,
   biography: data.biography,
-  birthday: data.birthday,
+  birthday: getBirthday(data.birthday),
   deathday: data.deathday,
   place_of_birth: data.place_of_birth,
   profile_path: data.profile_path,
