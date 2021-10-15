870b02019c4a1d6be457e9878a8f4043306c3d56
Yaiza <yaizavallejo16@gmail.com>
2021-10-15 12:13:38 +0200

    Add a array with some tv programms in order to not get recommended

--- src/helpers/movieInfo/getRecommendations.js
+++ src/helpers/movieInfo/getRecommendations.js
@@ -1,9 +1,20 @@
+const filterRecommendations = (data) => {
+  const notRecommendations = [
+    562, 59941, 2224, 1900, 1220, 1489, 1667, 62223, 63770, 4573,
+  ];
+  const recommendations = data.filter(
+    (item) => !notRecommendations.includes(item.id)
+  );
+  return recommendations;
+};
+
 const orderRecommendations = (data) => {
   const recommendations = data.cast.sort((a, b) => {
     if (a.popularity > b.popularity) return -1;
     return a.popularity < b.popularity ? 1 : 0;
   });
-  return recommendations;
+  // return recommendations;
+  return filterRecommendations(recommendations);
 };
 
 const getRecommendations = (data) => data.results;
