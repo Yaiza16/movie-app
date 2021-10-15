d2c1b0f6ff4f178b2a93c2d3989700d837d22513
Yaiza <yaizavallejo16@gmail.com>
2021-10-15 11:16:30 +0200

    Add components

--- src/pages/PersonPage.js
+++ src/pages/PersonPage.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import PersonalData from '../components/DetailsPerson/PersonalData';
 import ProfilePoster from '../components/DetailsPerson/ProfilePoster';
 import usePerson from '../hooks/usePerson';
+
+import './PersonPage.scss';
 
 const PersonPage = () => {
   const { id } = useParams();
@@ -18,9 +21,10 @@ const PersonPage = () => {
 
   return (
     <div className="details-page-container">
-      <div className="details-page">
+      <div className="details-page details-page--person">
         <div className="details-page-main">
           <ProfilePoster person={person} />
+          <PersonalData person={person} />
         </div>
       </div>
     </div>
