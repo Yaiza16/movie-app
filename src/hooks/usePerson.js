c88e68801844612b2e2b139eafe8562c15706f58
Yaiza <yaizavallejo16@gmail.com>
2021-10-15 11:46:43 +0200

    Add person recommendations

--- src/hooks/usePerson.js
+++ src/hooks/usePerson.js
@@ -1,11 +1,15 @@
 import axiosRequest from '../helpers/axios/axiosRequest';
+import { getRecommendationsPerson } from '../helpers/movieInfo/getRecommendations';
 import getPersonMainData from '../helpers/personInfo/getPersonMainData';
 
 const usePerson = () => {
   const getNewPerson = async (id, type) => {
     const personMainData = getPersonMainData(await axiosRequest(id, type));
+    const personRecommendations = getRecommendationsPerson(
+      await axiosRequest(id, type, 'combined_credits')
+    );
 
-    const variable = { ...personMainData };
+    const variable = { ...personMainData, personRecommendations };
     console.log(variable);
     return variable;
   };
