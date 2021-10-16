f420f0d2ee39f38441e63b15c584e65823e7592b
Yaiza <yaizavallejo16@gmail.com>
2021-10-16 15:47:15 +0200

    Add search page component

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchPage = () => {
+  return (
+    <div className="search-page-container">
+      <p>Typing your search...</p>
+    </div>
+  );
+};
+
+export default SearchPage;
