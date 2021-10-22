f9150b527f3ccfc93879c57d5f888560f228e8ed
Yaiza <yaizavallejo16@gmail.com>
2021-10-22 10:04:59 +0200

    Do burger with just style

---
+++ src/components/Header/NavMobile/NavMobile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavMobile.scss';
+
+const NavMobile = () => {
+  return (
+    <div className="burger">
+      <div className="burger__line" />
+      <div className="burger__line" />
+      <div className="burger__line" />
+    </div>
+  );
+};
+
+export default NavMobile;
