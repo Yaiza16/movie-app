82adcf6d397fa1e5313957952651c226372445ee
Yaiza <yaizavallejo16@gmail.com>
2021-10-23 19:39:36 +0200

    Fix error 'validateDOMNesting'

--- src/components/DetailsMovie/CastSection/CastSection.js
+++ src/components/DetailsMovie/CastSection/CastSection.js
@@ -6,18 +6,22 @@ const CastSection = ({ title, cast, star }) => {
   return cast && cast.length !== 0 ? (
     <div className="cast-container">
       <p className="cast-title">{title}</p>
-      <p className="cast-item">
+      <div className="cast-item">
         {cast.map((person, i, arr) => {
           if (arr.length - 1 === i) {
-            return star ? <CastItem person={person} last /> : ` ${person.name}`;
+            return star ? (
+              <CastItem key={person.id} person={person} last />
+            ) : (
+              ` ${person.name}`
+            );
           }
           return star ? (
-            <CastItem person={person} last={false} />
+            <CastItem key={person.id} person={person} last={false} />
           ) : (
             ` ${person.name},`
           );
         })}
-      </p>
+      </div>
     </div>
   ) : null;
 };
