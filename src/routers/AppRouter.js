e6d937382481215b0cb96ff19f90f54079392a29
Yaiza <yaizavallejo16@gmail.com>
2021-10-23 18:07:07 +0200

    Delete comments and fix imports

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,24 +1,18 @@
-// import { LoaderProvider } from 'contexts/LoaderContext';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Loader from 'vendors/Loader/Loader';
-// import Loader from 'vendors/Loader/Loader';
-import Footer from '../components/Footer';
-import Header from '../components/Header/Header';
-import { FormProvider } from '../contexts/FormContext';
-import ScrollToTop from '../hooks/ScrollToTop';
-import DetailsPage from '../pages/DetailsPage';
-import MoviesPage from '../pages/MoviesPage';
-import PersonDetailsPage from '../pages/PersonDetailsPage';
-import SearchPage from '../pages/SearchPage';
-import SeriesPage from '../pages/SeriesPage';
-import MainPage from '../pages/MainPage';
-import './AppRouter.scss';
 
-// const MainPage = React.lazy(() => import('pages/MainPage'));
-// const Header = React.lazy(() => import('components/Header/Header'));
-// const MoviesPage = React.lazy(() => import('pages/MoviesPage'));
-// const DetailsPage = React.lazy(() => import('pages/DetailsPage'));
+import Footer from 'components/Footer';
+import Header from 'components/Header/Header';
+import { FormProvider } from 'contexts/FormContext';
+import ScrollToTop from 'hooks/ScrollToTop';
+import DetailsPage from 'pages/DetailsPage';
+import MoviesPage from 'pages/MoviesPage';
+import PersonDetailsPage from 'pages/PersonDetailsPage';
+import SearchPage from 'pages/SearchPage';
+import SeriesPage from 'pages/SeriesPage';
+import MainPage from 'pages/MainPage';
+
+import './AppRouter.scss';
 
 const AppRouter = () => {
   const [multiSearch, setMultiSearch] = useState('');
@@ -47,11 +41,9 @@ const AppRouter = () => {
             <Route exact path="/person/:id">
               <PersonDetailsPage />
             </Route>
-            {/* <Suspense fallback={<Loader />}> */}
-            <Route exact path="/" component={MainPage}>
+            <Route exact path="/">
               <MainPage />
             </Route>
-            {/* </Suspense> */}
           </Switch>
         </FormProvider>
         <Footer />
